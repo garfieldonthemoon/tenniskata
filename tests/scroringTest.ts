612d5c71ed534fd7f4a6ed0f7ecd972b47df5fc4
David Viktor Koltai <dkoltai@Davids-MacBook-Pro.local>
2020-03-23 14:18:36 +0700

    modify test name to be more descriptive

--- tests/scroringTest.ts
+++ tests/scroringTest.ts
@@ -5,7 +5,7 @@ describe('scoring is accurate for', function() {
     using(0, 0, 'love-all').
     using(15, 15, 'fifteen-all').
     using(30, 30, 'thirty-all').
-        it('equal scores', function(scorePlayerLeft, scorePlayerRight, expectedScoring) {
+        it('equal scores up to 30', function(scorePlayerLeft, scorePlayerRight, expectedScoring) {
             //arrange
             let game = new TennisGame();
             game.setScore(scorePlayerLeft);
